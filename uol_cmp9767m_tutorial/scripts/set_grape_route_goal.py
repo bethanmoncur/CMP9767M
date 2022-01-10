5761f6acd4db59e343e2f44a7d5703293691102a
Bethan <bethanmoncur@gmail.com>
2022-01-10 21:57:34 +0000

    Editing launch file

--- uol_cmp9767m_tutorial/scripts/set_grape_route_goal.py
+++ uol_cmp9767m_tutorial/scripts/set_grape_route_goal.py
@@ -11,7 +11,7 @@ import actionlib
 from topological_navigation.msg import GotoNodeAction, GotoNodeGoal
 
 if __name__ == '__main__':
-    rospy.init_node('topological_navigation_client')
+    rospy.init_node('grape_topological_navigation')
     client = actionlib.SimpleActionClient('/thorvald_001/topological_navigation', GotoNodeAction)
     client.wait_for_server()
 
