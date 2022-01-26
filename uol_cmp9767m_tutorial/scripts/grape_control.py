6683d70045e135591b79efc734032341abe225ee
Bethan <bethanmoncur@gmail.com>
2022-01-26 16:51:13 +0000

    Comments to control script and localise script

--- uol_cmp9767m_tutorial/scripts/grape_control.py
+++ uol_cmp9767m_tutorial/scripts/grape_control.py
@@ -1,30 +1,26 @@
 #! /usr/bin/env python
-# ----------------------------------
-# @author: gpdas
-# @email: pdasgautham@gmail.com
-# @date:
-# ----------------------------------
 
+#import packages
 import rospy
-
 from std_msgs.msg import String, Int32
 
 
 class grape_control:
     
+
     def __init__(self):
+
         # subscribe to get information about topological navigation 
         self.nav_sub = rospy.Subscriber('navigation_control', String, self.nav_callback)
+        # subscribe to get status of counting to continue navigation after reset
+        self.counting_status_sub = rospy.Subscriber('counting_status', String, self.resume_nav_callback)
+        # subscribe to get the number of grapes
+        self.grape_sub =  rospy.Subscriber('grape_count', Int32, self.count_grapes)
+        # subscribe to get input from user about localisation
+        self.localise_sub = rospy.Subscriber('localisation_complete', String, self.resume_nav_callback)
 
         # publish commands to contol the grape counting process
         self.control_pub = rospy.Publisher('counting_control', String, queue_size=10)
-
-        # subscribe to get the number of grapes
-        self.grape_sub =  rospy.Subscriber('grape_count', Int32, self.count_grapes)
-
-        # subscribe to get status of counting to continue navigation after reset
-        self.counting_status_sub = rospy.Subscriber('counting_status', String, self.resume_nav_callback)
-
         # publish commands to the navigation goal setter to resume navigation
         self.nav_pub = rospy.Publisher('navigation_resume', String, queue_size=10)
 
@@ -32,10 +28,8 @@ class grape_control:
         self.results = []
         self.average_count = 0
 
-        # subscribe to get input from user about localisation
-        self.localise_sub = rospy.Subscriber('localisation_complete', String, self.resume_nav_callback)
 
-
+    # function to pause the counting process and resume navigation once localisation or grape counting is complete
     def resume_nav_callback(self, data):
         if 'counting complete' in str(data) or 'localisation complete' in str(data):
             self.control_pub.publish('pause count')
@@ -43,37 +37,39 @@ class grape_control:
             print 'pause counting grapes, resume navigation'
 
 
+    # function to start and stop counting depending on the location of the robot
     def nav_callback(self, data):
         # data is in the form (data: "WayPoint1,success: True")
         data_string = str(data).split('"')
+        # get the current waypoint from the data string
         self.current_waypoint = data_string[1].split(',')[0]
-        status = data_string[1].split(': ')[1]
-        if status == 'True':
-            self.waypoint_reached = True
-        else:
-            self.waypoint_reached = False
-        
         print 'Current waypoint: ', self.current_waypoint
-        print 'Waypoint reached: ', self.waypoint_reached
 
+        # define the waypoints for counting to take place at
         active_waypoints = ['WayPoint1', 'WayPoint2', 'WayPoint3', 'WayPoint4', 'WayPoint6', 'WayPoint7', 'WayPoint8', 'WayPoint9']
+
+        # define the waypoints to reset the grape count at
         reset_waypoints = ['WayPoint5', 'WayPointX']
         
-
+        # start the counting process if the robot is at one of the correct waypoints
         if self.current_waypoint in active_waypoints:
             print 'start counting grapes'
             self.control_pub.publish('start count')
         
+        # reset the grape count if the robot is at one of the correct waypoints
         if self.current_waypoint in reset_waypoints:
             print 'reset count'
             self.control_pub.publish('reset count')
 
 
-
+    # function to print the number of grapes counted and calculate the average number over both directions
     def count_grapes(self, data):
         count = int(str(data).split(': ')[1])
+        # save the count in a list
         self.results.append(count)
+        # calculate the average
         self.average_count =  float(sum(self.results)) / float(len(self.results))
+        # display the results
         print 'Current count: ', count
         print 'List of counts: ', self.results
         print 'Average count: ', self.average_count
--- uol_cmp9767m_tutorial/scripts/grape_localise.py
+++ uol_cmp9767m_tutorial/scripts/grape_localise.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 
+#import packages
 import rospy
-
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point, Twist
 from std_srvs.srv import Empty
 from std_msgs.msg import String
-
 from tf.transformations import euler_from_quaternion
 from math import atan2
 
+# create global variables to define the current position of the robot from odometry    
 x = 0.0
 y = 0.0
 theta = 0.0
 
+
+# funtion to update the position of the robot from odometry data    
 def new_odom(data):
     global x
     global y
@@ -24,16 +26,25 @@ def new_odom(data):
     rot_q = data.pose.pose.orientation
     (roll, pitch, theta) = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
     
+
 rospy.init_node('move_to_localise', anonymous=True)
 
+# subscribe to the odometry topic
 odom_sub = rospy.Subscriber('/thorvald_001/odometry/base_raw', Odometry, new_odom)
+
+# publish velocity commands to the robot
 vel_pub = rospy.Publisher('/thorvald_001/twist_mux/cmd_vel', Twist, queue_size = 1)
+
+# publish feedback to the control node
 localise_pub = rospy.Publisher('localisation_complete', String, queue_size = 1)
+
+# create a service proxy to enable redistributing of the amcl particles
 global_localization = rospy.ServiceProxy('global_localization', Empty)
 
-
+# ask the user if the robot needs localising
 start_input = raw_input('Does the robot need localising? [y/n] \n')
-    
+
+# tell the control node that the robot is localised    
 if start_input == 'n':
     localise_pub.publish('localisation complete')
     quit()
@@ -42,24 +53,29 @@ if start_input == 'n':
 #else:
     #global_localization()
 
+# create an object to send velocity commands  
 t = Twist()
 
+# define a rate of 10hz
 r = rospy.Rate(10)
 
+# define the odometry targets to send to the robot
 i = 0
 goals = [[-8.0, -3.0], [0.0, 0.0], [5.0, -1.0], [-5.0, 1.0]]
 
 while not rospy.is_shutdown():
 
-
+    # set the goal location
     goal = Point()
     goal.x = goals[i][0]
     goal.y = goals[i][1]
     
+    # calculate the heading change required to get to the target position 
     dif_x = goal.x - x
     dif_y = goal.y - y
     angle_to_goal = atan2(dif_y, dif_x)
     
+    # move the robot towards the goal if it is further than 1m away in either the x or y direction 
     if abs(dif_x) > 1.0 or abs(dif_y) > 1.0:
     
         if (angle_to_goal - theta) > 0.1:
@@ -71,21 +87,24 @@ while not rospy.is_shutdown():
         else:
             t.linear.x = 0.8
             t.angular.z = 0.0
-        
+        # publish the velocity command
         vel_pub.publish(t)
     
     else:
+        # ask the user if the robot is localised
         user_input = raw_input('Has the robot successfully localised itself? [y/n] \n')
         if user_input == 'n':
+            # redistribute the amcl particles and move to the next goal
             global_localization()
             if i == len(goals) - 1:
                 i = 0
             else:
                 i +=1
         elif user_input == 'y':
+            # feedback to the control node
             localise_pub.publish('localisation complete')
             break
             
-        
+    # complete the while loop at a rate of 10 hz    
     r.sleep()
 
